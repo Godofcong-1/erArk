48382da9fe4b8c5ef18fd678ad40a2b597b37e54
Godofcong-1 <381123017@qq.com>
2022-09-20 05:24:09 +0800

    污浊界面会有对应的显示信息

--- Script/UI/Panel/dirty_panel.py
+++ Script/UI/Panel/dirty_panel.py
@@ -29,6 +29,8 @@ class Dirty_Panel:
         """初始化绘制对象"""
         self.width: int = width
         """ 绘制的最大宽度 """
+        self.now_panel = _("身体")
+        """ 当前绘制的页面 """
         self.draw_list: List[draw.NormalDraw] = []
         """ 绘制的文本列表 """
 
@@ -38,7 +40,8 @@ class Dirty_Panel:
         character_data: game_type.Character = cache.character_data[0]
         target_data: game_type.Character = cache.character_data[character_data.target_character_id]
 
-        title_text = target_data.name + "身体部位污浊情况"
+        title_text = target_data.name + "污浊情况"
+        dirty_type_list = [_("身体"), _("服装")]
 
         title_draw = draw.TitleLineDraw(title_text, self.width)
         handle_panel = panel.PageHandlePanel([], Dirty_Draw, 10, 5, self.width, 1, 1, 0)
@@ -46,31 +49,70 @@ class Dirty_Panel:
             return_list = []
             title_draw.draw()
 
+            # 绘制主面板
+            for dirty_type in dirty_type_list:
+                if dirty_type == self.now_panel:
+                    now_draw = draw.CenterDraw()
+                    now_draw.text = f"[{dirty_type}]"
+                    now_draw.style = "onbutton"
+                    now_draw.width = self.width / len(dirty_type_list)
+                    now_draw.draw()
+                else:
+                    now_draw = draw.CenterButton(
+                        f"[{dirty_type}]",
+                        dirty_type,
+                        self.width / len(dirty_type_list),
+                        cmd_func=self.change_panel,
+                        args=(dirty_type,),
+                    )
+                    now_draw.draw()
+                    return_list.append(now_draw.return_text)
+            line_feed.draw()
+            line = draw.LineDraw("+", self.width)
+            line.draw()
+
+
             name_draw = draw.NormalDraw()
 
             # 遍历全部位并输出结果
             # 部位列表[0"头发",1"脸部",2"口腔",3"胸部",4"腋部",5"手部",6"小穴",7"后穴",8"尿道",9"腿部",10"脚部",11"尾巴",12"兽角",13"兽耳"]
             # 服装列表[0"帽子",1"眼镜",2"耳部",3"脖子",4"嘴部",5"上衣",6"内衣（上）",7"手套",8"下衣",9"内衣（下）",10"袜子",11"鞋子",12"武器",13"附属物1",14"附属物2",15"附属物3",16"附属物4",17"附属物5"]
-            a_clean_list = [" <脏污>"," <灌肠中>"," <已灌肠清洁过>"," <精液灌肠中>"," <已精液灌肠过>"]
-            now_text = "\n"
-            for i in range(11):
-                now_text += "  " + target_data.dirty.body_semen[i][0] + "："
-                if i == 7:
-                    a_clean_text = a_clean_list[target_data.dirty.a_clean]
-                    now_text += a_clean_text
-                if i == 8:
-                    if target_data.urinate_point <= 30:
-                        now_text += " <残留的尿渍>"
-                    elif target_data.urinate_point <= 120:
-                        now_text += " <洁净>"
-                    elif target_data.urinate_point <= 191:
-                        now_text += " <些许尿意>"
-                    elif target_data.urinate_point >= 192:
-                        now_text += " <充盈的尿意>"
-                if target_data.dirty.body_semen[i][1] != 0:
-                    now_text += " <" + attr_text.get_semen_now_text(target_data.dirty.body_semen[i][2],0) + ">(" + str(target_data.dirty.body_semen[i][1]) + "ml)"
+            if self.now_panel == "身体":
+                a_clean_list = [" <脏污>"," <灌肠中>"," <已灌肠清洁过>"," <精液灌肠中>"," <已精液灌肠过>"]
+                now_text = "\n"
+                for i in range(11):
+                    now_text += "  " + target_data.dirty.body_semen[i][0] + "："
+                    if i == 7:
+                        a_clean_text = a_clean_list[target_data.dirty.a_clean]
+                        now_text += a_clean_text
+                    if i == 8:
+                        if target_data.urinate_point <= 30:
+                            now_text += " <残留的尿渍>"
+                        elif target_data.urinate_point <= 120:
+                            now_text += " <洁净>"
+                        elif target_data.urinate_point <= 191:
+                            now_text += " <些许尿意>"
+                        elif target_data.urinate_point >= 192:
+                            now_text += " <充盈的尿意>"
+                    if target_data.dirty.body_semen[i][1] != 0:
+                        now_text += " <" + attr_text.get_semen_now_text(target_data.dirty.body_semen[i][2],0) + ">(" + str(target_data.dirty.body_semen[i][1]) + "ml)"
+                    now_text += "\n"
                 now_text += "\n"
-            now_text += "\n"
+
+            elif self.now_panel == "服装":
+
+                now_text = ""
+                for clothing_type in game_config.config_clothing_type:
+                    type_name = game_config.config_clothing_type[clothing_type].name
+                    if len(target_data.cloth[clothing_type]):
+                        now_text += f"  [{type_name}]:"
+                        for cloth_id in target_data.cloth[clothing_type]:
+                            cloth_name = game_config.config_clothing_tem[cloth_id].name
+                            now_text += f" {cloth_name}"
+                            if target_data.dirty.cloth_semen[clothing_type][1] != 0:
+                                now_text += f"({str(target_data.dirty.cloth_semen[clothing_type][1])}ml)"
+                        now_text += "\n"
+
 
             name_draw.text = now_text
             name_draw.width = self.width
@@ -87,6 +129,19 @@ class Dirty_Panel:
                 break
 
 
+    def change_panel(self, dirty_type: str):
+        """
+        切换当前面板显示
+        Keyword arguments:
+        dirty_type -- 要切换的面板类型
+        """
+
+
+        if dirty_type == "身体":
+            self.now_panel = "身体"
+
+        elif dirty_type == "服装":
+            self.now_panel = "服装"
 
 
 class Dirty_Draw:
