d621d71b66ddd245174987509a564685db7de7ca
cong <381123017@qq.com>
2022-11-04 18:32:18 +0800

    还没写完的加单条前提

--- buildata.py
+++ buildata.py
@@ -5,13 +5,15 @@ from Script.Config import game_config
 '''
 
 
-# 1前提生成到py里,2前提文件转csv,3csv转前提文件
-mode = 2
+# 1单条前提生成到三个文件里,2前提文件转csv,3csv转前提文件
+mode = 1
 command_str = "t_lubrication_l_7"
 capital_command = command_str.upper()
 dataname = "润滑"
-# A:能力数值,S:状态数值,T:素质
 datetype = "S"
+premise_type_diy = "自定义内容"
+premise_type_dict = {"A":"属性_能力","S":"属性_状态","T":"属性_素质","X":premise_type_diy}
+premise_type = premise_type_dict[datetype]
 
 # 数据初始化
 game_config.init()
@@ -40,7 +42,9 @@ def constand_2_handle():
             if game_config.config_talent[config_data_index].name == dataname:
                 data_index = config_data_index
                 break
-    print(f"数据名 = {dataname}，类型 = {datetype_str}，id = {config_data_index}")
+    elif datetype == "X":
+        datetype_str,data_index = "数据类型","数据序号"
+    print(f"数据名 = {dataname}，类型 = {datetype_str}，id = {data_index}")
 
     command_list = command_str.split('_')
 
@@ -67,14 +71,29 @@ def constand_2_handle():
             target_flag = True
             break
 
-    # 进行变量名输出
+    # 生成constant_promise的out_str
     out_str = "\n"
-    out_str += f"    {capital_command} = \"{command_str}\"\n"
-    if not target_flag:
-        out_str += f"    \"\"\" {dataname}{operation}{command_list[-1]} \"\"\"\n"
-    else:
-        out_str += f"    \"\"\" 交互对象{dataname}{operation}{command_list[-1]} \"\"\"\n"
+    out_str += f"    {command_str.upper()} = \"{command_str}\"\n"
+    out_str += f"    \"\"\" {premise_type} 交互对象{dataname}{operation}{command_list[-1]} \"\"\"\n"
     print(out_str)
+
+    # 输出到constant_promise里
+    break_flag = 0
+    with open("Script\\Core\\constant_promise.py", "r+",encoding="utf-8") as f:
+        while 1:
+            a=f.readline() # 逐行读取
+            if len(a) >= 3 and a[-3] == "\"": # 锁定到文字行
+                promise_text = a.split("\"")[-4].strip()
+                if promise_text != "前提id":
+                    # print(f"debug promise_text = {promise_text}")
+                    promise_type,promise_info = promise_text.split(" ")[0],promise_text.split(" ")[1]
+                    if promise_type == premise_type:
+                        break_flag = 1
+                    if break_flag == 1 and promise_type != premise_type:
+                        f.write(out_str)
+                        break
+        f.close()
+    out_str = "\n"
 
     # 进行前提输出
     out_str = "\n\n"
@@ -93,15 +112,15 @@ def constand_2_handle():
     out_str += f"    character_data = cache.character_data[character_id]\n"
     if target_flag:
         out_str += f"    target_data = cache.character_data[character_data.target_character_id]\n"
-        out_str += f"    if target_data.{datetype_str}[{config_data_index}] {operation} {command_list[-1]}:\n"
+        out_str += f"    if target_data.{datetype_str}[{data_index}] {operation} {command_list[-1]}:\n"
     else:
-        out_str += f"    if character_data.{datetype_str}[{config_data_index}] {operation} {command_list[-1]}:\n"
+        out_str += f"    if character_data.{datetype_str}[{data_index}] {operation} {command_list[-1]}:\n"
     out_str += f"        return 1\n"
     out_str += f"    return 0\n"
 
     # 开始保存
     with open("Script\\Design\\handle_premise.py", "a",encoding="utf-8") as f:
-        f.write(out_str)
+        # f.write(out_str)
         f.close()
     print(f"已写入前提文件末尾")
 
@@ -111,7 +130,6 @@ def constand_2_csv():
     with open("Script\\Core\\constant_promise.py", "r",encoding="utf-8") as f:
         a=f.readlines()
         f.close()
-    promise_text_list,promise_type_list,promise_cid_list = [],[],[]
     out_str = "\n"
 
     for line in a:
