f1767ac1f3446b41ffba4279b9528db50eb1f4c4
cong_yudie_4070ti <381123017@qq.com>
2023-09-07 14:26:24 +0800

    加了debug里的助理情况

--- Script/UI/Panel/debug_panel.py
+++ Script/UI/Panel/debug_panel.py
@@ -263,7 +263,7 @@ class Debug_Panel:
             draw_text_list.append(f"[011]:初次状态记录")
             draw_text_list.append(f"[012]:污浊（已实装）")
             draw_text_list.append(f"[013]:本次H")
-            draw_text_list.append(f"[014]:助理情况")
+            draw_text_list.append(f"[014]:助理情况（已实装）")
             draw_text_list.append(f"[015]:行动记录")
             draw_text_list.append(f"[016]:工作")
             draw_text_list.append(f"[017]:娱乐（已实装）")
@@ -813,6 +813,43 @@ class Debug_Panel:
                     # 接着刷新一遍显示新内容
                     change_draw_flag = False
                     continue
+
+
+            # 助理数据
+            elif key_index == 14:
+                draw_text_list = []
+                info_text = f"\n"
+                for cid in target_data.assistant_services:
+                    name = game_config.config_assistant_services[cid].name
+                    info_text += f"{cid}:{name}={target_data.assistant_services[cid]} "
+                draw_text_list.append(f"[000]:服务列表：\n{info_text}")
+
+                # 进行显示
+                for i in range(len(draw_text_list)):
+                    info_draw.text = draw_text_list[i]
+                    info_draw.draw()
+                    line_feed.draw()
+
+                # 如果需要输入，则进行两次输入
+                if change_draw_flag:
+                    line_feed.draw()
+                    value_index_panel = panel.AskForOneMessage()
+                    value_index_panel.set(_("输入改变第几项，如果是带子项的项的话，中间用英文小写逗号隔开"), 100)
+                    value_index = value_index_panel.draw()
+                    if "," in value_index: # 转成全int的list
+                        value_index = list(map(int, value_index.split(",")))
+                    else:
+                        value_index = int(value_index)
+                    change_value_panel = panel.AskForOneMessage()
+                    change_value_panel.set(_("输入改变后的值"), 100)
+                    new_value = int(change_value_panel.draw())
+
+                    target_data.assistant_services[value_index] = new_value
+
+                    # 接着刷新一遍显示新内容
+                    change_draw_flag = False
+                    continue
+
 
             # 娱乐数据
             elif key_index == 17:
