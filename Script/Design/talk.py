2923c5247558fc220d3b8e77fea61e0c641869f0
cong_yudie_4070ti <381123017@qq.com>
2024-06-20 16:02:12 +0800

    修正：修正了会显示非博士当前地点的干员的绝顶信息的BUG

--- Script/Design/talk.py
+++ Script/Design/talk.py
@@ -193,10 +193,7 @@ def must_show_talk_check(character_id: int):
         if behavior_data != 0:
             # print(f"debug 检测到{second_behavior_id}可能需要显示")
             # 需要有必须显示
-            if (
-                998 in game_config.config_second_behavior_effect_data[second_behavior_id]
-                or second_behavior_id not in range(1100,1113)
-                ):
+            if 998 in game_config.config_second_behavior_effect_data[second_behavior_id]:
                 now_talk_data = handle_talk_sub(character_id, second_behavior_id, True)
                 # 触发后该行为值归零
                 character_data.second_behavior[second_behavior_id] = 0
