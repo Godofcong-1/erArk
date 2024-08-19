7e141532cd8f1ea20ac2afd72cef74d48879cd45
cong_yudie_4070ti <381123017@qq.com>
2024-08-19 14:40:07 +0800

    修正：修正了在衣柜中偷取内裤和袜子时会将衣物统计为最后一位偷窃的角色的BUG

--- Script/UI/Panel/check_locker_panel.py
+++ Script/UI/Panel/check_locker_panel.py
@@ -140,6 +140,7 @@ class FindDraw:
         return_list = []
         # title_draw.draw()
         cloth_show_draw = draw.NormalDraw()
+        self.pl_data.target_character_id = self.npc_id
         while 1:
             line = draw.LineDraw("-", window_width)
             line.draw()
@@ -259,8 +260,8 @@ class FindDraw:
 
         pan_id = self.character_data.cloth.cloth_locker[9][-1]
         pan_name = game_config.config_clothing_tem[pan_id].name
-        self.pl_data.pl_collection.npc_panties_tem.setdefault(self.pl_data.target_character_id, [])
-        self.pl_data.pl_collection.npc_panties_tem[self.pl_data.target_character_id].append(pan_id)
+        self.pl_data.pl_collection.npc_panties_tem.setdefault(self.npc_id, [])
+        self.pl_data.pl_collection.npc_panties_tem[self.npc_id].append(pan_id)
         self.character_data.cloth.cloth_locker[9] = []
         now_draw = draw.WaitDraw()
         now_draw.width = window_width
@@ -272,8 +273,8 @@ class FindDraw:
 
         socks_id = self.character_data.cloth.cloth_locker[10][-1]
         socks_name = game_config.config_clothing_tem[socks_id].name
-        self.pl_data.pl_collection.npc_socks_tem.setdefault(self.pl_data.target_character_id, [])
-        self.pl_data.pl_collection.npc_socks_tem[self.pl_data.target_character_id].append(socks_id)
+        self.pl_data.pl_collection.npc_socks_tem.setdefault(self.npc_id, [])
+        self.pl_data.pl_collection.npc_socks_tem[self.npc_id].append(socks_id)
         self.character_data.cloth.cloth_locker[10] = []
         now_draw = draw.WaitDraw()
         now_draw.width = window_width
