4f628ade5d03978847b498cb82a2f122bc82843f
cong_yudie_4070ti <381123017@qq.com>
2024-11-14 18:30:54 +0800

    修正：修正了在宿舍衣柜中存在衣物种类但没有衣物数据的情况下，会导致干员不穿衣服的BUG

--- Script/Design/clothing.py
+++ Script/Design/clothing.py
@@ -48,19 +48,26 @@ def get_cloth_from_dormitory_locker(character_id: int):
     """
     if character_id:
         character_data = cache.character_data[character_id]
-        # 宿舍衣柜中有衣服的话，穿上衣服
+        # 检查宿舍衣柜中是否有衣服
+        wear_locker_flag = False
         if len(character_data.cloth.cloth_locker_in_dormitory):
+            for cloth_type in character_data.cloth.cloth_locker_in_dormitory:
+                if len(character_data.cloth.cloth_locker_in_dormitory[cloth_type]):
+                    wear_locker_flag = True
+                    break
+        # 宿舍衣柜中有衣服的话，穿上衣服
+        if wear_locker_flag:
             character_data.cloth.cloth_wear = attr_calculation.get_cloth_wear_zero()
             character_data.cloth.cloth_wear = character_data.cloth.cloth_locker_in_dormitory
             character_data.cloth.cloth_locker_in_dormitory = attr_calculation.get_cloth_locker_in_dormitory_zero()
             # 换内衣内裤
             get_underwear(character_id)
+            # 将衣柜里的衣服精液转移到穿着的衣服上
+            locker_cloth_semen_to_wear_cloth_semen(character_id)
         # 宿舍衣柜中没有衣服的话，根据csv穿衣服
-        else:
+        if not wear_locker_flag:
             get_npc_cloth(character_id)
 
-    # 将衣柜里的衣服精液转移到穿着的衣服上
-    locker_cloth_semen_to_wear_cloth_semen(character_id)
     # 穿特殊服装
     chara_special_wear_cloth(character_id)
 
