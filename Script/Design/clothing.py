2a6c98f60b3059e80370d5b4cd691582bbd87d65
cong_yudie_4070ti <381123017@qq.com>
2024-11-18 15:30:10 +0800

    修正：修正了宿舍的衣柜中可能会存在非角色人物卡中设定的衣物的BUG

--- Script/Design/clothing.py
+++ Script/Design/clothing.py
@@ -48,13 +48,16 @@ def get_cloth_from_dormitory_locker(character_id: int):
     """
     if character_id:
         character_data = cache.character_data[character_id]
+        tem_character = cache.npc_tem_data[character_id - 1]
         # 检查宿舍衣柜中是否有衣服
         wear_locker_flag = False
-        if len(character_data.cloth.cloth_locker_in_dormitory):
-            for cloth_type in character_data.cloth.cloth_locker_in_dormitory:
-                if len(character_data.cloth.cloth_locker_in_dormitory[cloth_type]):
-                    wear_locker_flag = True
-                    break
+        for cloth_type in character_data.cloth.cloth_locker_in_dormitory:
+            if len(character_data.cloth.cloth_locker_in_dormitory[cloth_type]):
+                wear_locker_flag = True
+                # 去掉里面不符合角色csv的衣服
+                for cloth_id in character_data.cloth.cloth_locker_in_dormitory[cloth_type].copy():
+                    if cloth_id not in tem_character.Cloth:
+                        character_data.cloth.cloth_locker_in_dormitory[cloth_type].remove(cloth_id)
         # 宿舍衣柜中有衣服的话，穿上衣服
         if wear_locker_flag:
             character_data.cloth.cloth_wear = attr_calculation.get_cloth_wear_zero()
