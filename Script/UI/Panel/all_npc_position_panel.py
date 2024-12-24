43ec6c71fb0a83e31b537315ffda6f0164cf0308
cong_yudie_4070ti <381123017@qq.com>
2024-12-24 10:59:55 +0800

    修正：修正了查看干员位置面板中选择了前往干员位置模式时点击下一页会跳出的BUG

--- Script/UI/Panel/all_npc_position_panel.py
+++ Script/UI/Panel/all_npc_position_panel.py
@@ -168,7 +168,7 @@ class All_Npc_Position_Panel:
             return_list.append(back_draw.return_text)
             yrn = flow_handle.askfor_all(return_list)
             # 选择玩家移动时，也跳出循环
-            if yrn == back_draw.return_text or (yrn in self.handle_panel.return_list and self.move_type == 2):
+            if yrn == back_draw.return_text or (yrn in self.handle_panel.return_list and yrn not in {'0', '1'} and self.move_type == 2):
                 cache.now_panel_id = constant.Panel.IN_SCENE
                 break
 
