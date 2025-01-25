37a1bcc49f8cd02446874a0a8f6bdada08226af5
cong_yudie_4070ti <381123017@qq.com>
2025-01-25 15:33:18 +0800

    调整：玩家太过疲劳时不会再出现提示信息了

--- Script/Design/handle_npc_ai.py
+++ Script/Design/handle_npc_ai.py
@@ -112,14 +112,14 @@ def judge_character_tired_sleep(character_id : int):
         # 玩家只进行HP1的疲劳判定
         if character_data.hit_point <= 1:
             # 绘制文本
-            draw_text = character_data.name + _("太累了")
             if character_data.sp_flag.is_h:
+                draw_text = character_data.name + _("太累了")
                 draw_text = _("，无法继续H")
-            draw_text += "\n"
-            now_draw = draw.WaitDraw()
-            now_draw.width = window_width
-            now_draw.text = draw_text
-            now_draw.draw()
+                draw_text += "\n"
+                now_draw = draw.WaitDraw()
+                now_draw.width = window_width
+                now_draw.text = draw_text
+                now_draw.draw()
             # 数据处理
             character_data.sp_flag.tired = False
             target_data = cache.character_data[character_data.target_character_id]
