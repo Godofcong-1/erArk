b21ec6390c05e8a969f945c9319a43f2176b99c3
cong_yudie_4070ti <381123017@qq.com>
2025-05-14 14:32:20 +0800

    调整：在时停中使用无意识H指令时不会再进入关门面板了

--- Script/Design/handle_instruct.py
+++ Script/Design/handle_instruct.py
@@ -2122,7 +2122,7 @@ def handle_unconscious_h():
     character_data: game_type.Character = cache.character_data[0]
     target_data = cache.character_data[character_data.target_character_id]
     now_scene_str = map_handle.get_map_system_path_str_for_list(character_data.position)
-    if cache.scene_data[now_scene_str].close_flag == 0:
+    if cache.scene_data[now_scene_str].close_flag == 0 and handle_premise.handle_time_stop_off(0):
         now_draw = normal_panel.Close_Door_Panel(width)
         door_return = now_draw.draw()
         if door_return == -1:
