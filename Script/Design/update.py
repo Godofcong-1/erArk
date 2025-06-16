5bbd77f025b89d6e509d6c1878db1921c24d42f9
cong_yudie_4070ti <381123017@qq.com>
2025-06-16 10:35:37 +0800

    调整：增加了游戏时间步进时的防递归死循环机制

--- Script/Design/update.py
+++ Script/Design/update.py
@@ -1,6 +1,6 @@
-import logging, time
-from Script.Design import character_behavior, game_time, event
-from Script.Core import py_cmd
+import logging
+from Script.Design import character_behavior, game_time
+from Script.Core import py_cmd, cache_control
 
 
 def game_update_flow(add_time: int):
@@ -9,8 +9,19 @@ def game_update_flow(add_time: int):
     Keyword arguments:
     add_time -- 游戏步进的时间
     """
-    # 去掉了第一次结算
-    # character_behavior.init_character_behavior()
-    game_time.sub_time_now(add_time)
-    character_behavior.init_character_behavior()
-    py_cmd.focus_cmd()
+    # 检查是否已经在游戏更新流程中，防止递归调用导致死循环
+    if cache_control.cache.game_update_flow_running:
+        return
+    
+    # 设置游戏更新流程运行标志
+    cache_control.cache.game_update_flow_running = True
+    
+    try:
+        # 去掉了第一次结算
+        # character_behavior.init_character_behavior()
+        game_time.sub_time_now(add_time)
+        character_behavior.init_character_behavior()
+        py_cmd.focus_cmd()
+    finally:
+        # 无论是否发生异常，都要清除运行标志
+        cache_control.cache.game_update_flow_running = False
