4ab3b9b94a39259a0023b1dbdbc85c7d4850c2e6
cong_yudie_4070ti <381123017@qq.com>
2025-10-31 16:31:40 +0800

    修正：修正了加料咖啡的地文中混入精液的文本的前提错误的问题

--- Script/Design/handle_premise/handle_premise_other.py
+++ Script/Design/handle_premise/handle_premise_other.py
@@ -2792,6 +2792,94 @@ def handle_pl_action_food_medicine(character_id: int) -> int:
     return 0
 
 
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_BIRTH_CONTROL_AFTER)
+def handle_pl_action_food_birth_control_after(character_id: int) -> int:
+    """
+    校验食物调味_加事后避孕药
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 102:
+        return 1
+    return 0
+
+
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_PHILTER)
+def handle_pl_action_food_philter(character_id: int) -> int:
+    """
+    校验食物调味_加媚药
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 103:
+        return 1
+    return 0
+
+
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_DIURETICS_ONCE)
+def handle_pl_action_food_diuretics_once(character_id: int) -> int:
+    """
+    校验食物调味_加一次性利尿剂
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 105:
+        return 1
+    return 0
+
+
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_DIURETICS_PERSISTENT)
+def handle_pl_action_food_diuretics_persistent(character_id: int) -> int:
+    """
+    校验食物调味_加持续性利尿剂
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 106:
+        return 1
+    return 0
+
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_SLEEPING_PILLS)
+def handle_pl_action_food_sleeping_pills(character_id: int) -> int:
+    """
+    校验食物调味_加安眠药
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 107:
+        return 1
+    return 0
+
+
+@add_premise(constant_promise.Premise.PL_ACTION_FOOD_CLOMID)
+def handle_pl_action_food_clomid(character_id: int) -> int:
+    """
+    校验食物调味_加排卵促进药
+    Keyword arguments:
+    character_id -- 角色id
+    Return arguments:
+    int -- 权重
+    """
+    character_data = cache.character_data[0]
+    if character_data.behavior.food_seasoning == 108:
+        return 1
+    return 0
+
 @add_premise(constant_promise.Premise.AI_EAT_IN_RESTAURANT)
 def handle_ai_eat_in_restaurant(character_id: int) -> int:
     """
