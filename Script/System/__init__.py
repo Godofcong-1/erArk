bbf1da0e851b2e28b2da635e373ed8c4d32ac5c9
cong_yudie_4070ti <381123017@qq.com>
2025-12-04 19:24:37 +0800

    修正：修正了打包时没有将医疗经营系统的目录包含进去的BUG

--- Script/System/__init__.py
+++ Script/System/__init__.py
@@ -1,1 +1,3 @@
-"""显式标记成普通包"""
+"""Script.System package - 系统功能模块"""
+# 显式导入子包以便 PyInstaller 打包
+from . import Medical
--- Script/System/medical/__init__.py
+++ Script/System/medical/__init__.py
@@ -1,15 +1,15 @@
 """Medical subsystem package - 医疗系统子模块"""
-# 显式导入所有子模块以便 PyInstaller 打包
-from Script.System.Medical import (
+# 显式导入所有子模块以便 PyInstaller 打包（使用相对导入）
+from . import (
     medical_constant,
-    medical_service,
     medical_core,
-    medical_department_panel,
-    medical_player_diagnose_panel,
-    medical_player_diagnose_function,
-    hospital_doctor_service,
-    hospital_patient_management,
-    clinic_doctor_service,
-    clinic_patient_management,
     log_system,
+    medical_service,
+    medical_department_panel,
+    medical_player_diagnose_function,
+    medical_player_diagnose_panel,
+    clinic_patient_management,
+    clinic_doctor_service,
+    hospital_patient_management,
+    hospital_doctor_service,
 )
