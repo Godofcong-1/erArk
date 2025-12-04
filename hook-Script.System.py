5c0e06175163d58f332ae96367e206fb91e8faab
cong <381123017@qq.com>
2025-12-04 23:28:51 +0800

    修正：修正了打包时没有将医疗经营系统的目录包含进去的BUG

--- hook-Script.System.py
+++ hook-Script.System.py
@@ -1,6 +1,6 @@
-# PyInstaller hook for Script.System package
-# This hook ensures all Script.System submodules are collected
+# 用于解决 Script.System 及其子模块的隐藏导入问题
 
 from PyInstaller.utils.hooks import collect_submodules
 
 hiddenimports = collect_submodules('Script.System')
+hiddenimports += collect_submodules('Script.System.Medical')
